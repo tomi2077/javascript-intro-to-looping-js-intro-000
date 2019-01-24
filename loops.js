477867634f837c9033a20379d0b250c700336f64
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2019-01-24 14:32:35 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -2,6 +2,6 @@ function forLoop(array){
   for (var i = 1; i < 25; i++) {
     if (i===0) {array.push("I am 1 strange loop.")}
     else {array.push(`I am ${i} strange loops.`)}
-    return array
+    return array;
 }
 }
