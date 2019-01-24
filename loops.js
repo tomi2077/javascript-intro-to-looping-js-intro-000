c08d2ca1f3222368df5b2a9f4f41a0a8e2dca619
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2019-01-24 14:37:23 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,3 +1,17 @@
 function whileLoop(n) {
-  while n = 
+  while n = 0{
+    
+  }
 }
+
+describe('whileLoop(n)', () => {
+    it('counts down from n to 0', () => {
+      const spy = chai.spy.on(console, 'log')
+      const n = Math.floor(Math.random() * 100)
+
+      expect(whileLoop(n)).to.equal('done')
+      expect(spy).to.have.been.called.exactly(n)
+
+      console.log.reset()
+    })
+  })
