2c4b072128190d491a9cae079f7d04627f0ac713
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2019-01-24 14:41:44 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -18,4 +18,17 @@ function whileLoop(n) {
   return "done"
 }
 
+function doWhileLoop(array){
+  do {
+    array.pop;
+  }
+  while (array.length > 0)
+}
+
+do {
+  console.log("doo-bee-doo-bee-doo");
+  incrementVariable();
+} while (i < 5);
+```
+
 
